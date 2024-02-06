06f24d0be2839c79db30dae640b8fb3b46cff101
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-06 14:53:32 +0100

    test API boss

--- API.js
+++ API.js
@@ -1,7 +1,12 @@
 let button = document.getElementById("button");
 
 const changeboss = () => {
+        let randomnum = Math.ceil(Math.random * 59) + 1;
 
+        let requeststr = `https://mhw-db.com/monsters/${randomnum}`;
+
+        let data = fetch(requeststr);
+        console.log(data);
     
 }
 
