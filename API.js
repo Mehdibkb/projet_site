158415d84cf97602c233734677272cfa0c3d9e29
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-06 15:12:57 +0100

    test API reussi

--- API.js
+++ API.js
@@ -1,13 +1,13 @@
 let button = document.getElementById("button");
 
 const changeboss = async () => {
-        let randomnum = Math.ceil(Math.random * 59) + 1;
+    let randomnum = 1;
+//    let randomnum = Math.ceil(Math.random * 59) + 1;
 
         let requeststr = `https://mhw-db.com/monsters/${randomnum}`;
 
         let data = await fetch(requeststr);
-        console.log(data);
-        let reponse = data.json();
+        let reponse = await data.json();
         console.log(reponse);
     
 }
