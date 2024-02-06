8e476aa397a1c096219f5072bb39b34273b878cf
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-06 14:58:30 +0100

    test asynchrone

--- API.js
+++ API.js
@@ -1,11 +1,11 @@
 let button = document.getElementById("button");
 
-const changeboss = () => {
+const changeboss = async () => {
         let randomnum = Math.ceil(Math.random * 59) + 1;
 
         let requeststr = `https://mhw-db.com/monsters/${randomnum}`;
 
-        let data = fetch(requeststr);
+        let data = await fetch(requeststr);
         console.log(data);
     
 }
