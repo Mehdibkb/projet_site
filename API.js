3128aa5d9f289d7eee46d29c197e36a0c56900a1
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-10 18:02:29 +0100

    extraire les données de l'API

--- API.js
+++ API.js
@@ -1,20 +1,11 @@
-let button = document.getElementById("button");
-
-const changeboss = async () => {
-    let randomnum = 1;
-//    let randomnum = Math.ceil(Math.random * 59) + 1;
-
-        let requeststr = `https://mhw-db.com/monsters/${randomnum}`;
-
-        let data = await fetch(requeststr);
-        let reponse = await data.json();
-        console.log(reponse.name);
-        test = document.createElement('p');
-        test.textContent = reponse.name;
-        document.body.insertBefore(test, button.nextElementSibling);
-    
-}
-
-
-
-button.addEventListener("click" , changeboss)
+fetch("https://mhw-db.com/monsters")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+  })
