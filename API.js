6ba7c23bd71459c609f190c5c0a1112806157778
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-10 18:03:51 +0100

    extraire petits monstres de l'API

--- API.js
+++ API.js
@@ -8,4 +8,18 @@ fetch("https://mhw-db.com/monsters")
   })
   .then(data => {
     console.log(data);
-  })
+    petitmonstre(data)
+})
+
+function petitmonstre(data) {
+    for(n = 0; n < 17 ; n++ ){ 
+    const monstre_Nom = data[n];
+    const monstreDiv = document.getElementById("monsterList");
+    // monstre nom
+    const monstreName = monstre_Nom.name;
+    const heading = document.createElement("li");
+    heading.innerHTML = monstreName;
+    monstreDiv.className = 'content';
+    monstreDiv.appendChild(heading);
+  }
+  }
