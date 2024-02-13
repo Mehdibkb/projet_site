f9b6bc55fda6098f8a48d131270abe156d6d0081
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-13 11:38:52 +0100

    mise à jour du butoon menu

--- API.js
+++ API.js
@@ -1,37 +1,37 @@
-fetch("https://mhw-db.com/monsters")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then(data => {
-    console.log(data);
-    petitmonstre(data)
-    grandmonstre(data)
-})
+// fetch("https://mhw-db.com/monsters")
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("NETWORK RESPONSE ERROR");
+//     }
+//   })
+//   .then(data => {
+//     console.log(data);
+//     petitmonstre(data)
+//     grandmonstre(data)
+// })
 
-function petitmonstre(data) {
-    for(n = 0; n < 17 ; n++ ){ 
-    const monstre_Nom = data[n];
-    const monstreDiv = document.getElementById("monsterList");
+// function petitmonstre(data) {
+//     for(n = 0; n < 17 ; n++ ){ 
+//     const monstre_Nom = data[n];
+//     const monstreDiv = document.getElementById("monsterList");
     // monstre nom
-    const monstreName = monstre_Nom.name;
-    const heading = document.createElement("li");
-    heading.innerHTML = monstreName;
-    monstreDiv.appendChild(heading);
-  }
-  }
+//     const monstreName = monstre_Nom.name;
+//     const heading = document.createElement("li");
+//     heading.innerHTML = monstreName;
+//     monstreDiv.appendChild(heading);
+//   }
+//   }
 
-  function grandmonstre(data) {
-    for(n; n ; n++ ){ 
-    const monstre_Nom = data[n];
-    const monstreDiv2 = document.getElementById("grandmonstre");
+//   function grandmonstre(data) {
+//     for(n; n ; n++ ){ 
+//     const monstre_Nom = data[n];
+//     const monstreDiv2 = document.getElementById("grandmonstre");
     // monstre nom
-    const monstreName = monstre_Nom.name;
-    const heading = document.createElement("li");
-    heading.innerHTML = monstreName;
-    monstreDiv2.appendChild(heading);
-  }
-  }
+//     const monstreName = monstre_Nom.name;
+//     const heading = document.createElement("li");
+//     heading.innerHTML = monstreName;
+//     monstreDiv2.appendChild(heading);
+//   }
+//   }
