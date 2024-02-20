ad7937c8060caacd6b5388c2da6e1ca92339109d
Mehdi <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-02-20 20:43:41 +0100

    test de l API et ajout des noms des monstres

--- API.js
+++ API.js
@@ -10,40 +10,39 @@ menuburger.addEventListener('click', (event) => {
   event.stopPropagation(); // Arrête la propagation de l'événement
   navlinks.classList.toggle('mobile-menu');
 });
-// fetch("https://mhw-db.com/monsters")
-//   .then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error("NETWORK RESPONSE ERROR");
-//     }
-//   })
-//   .then(data => {
-//     console.log(data);
-//     petitmonstre(data)
-//     grandmonstre(data)
-// })
+fetch("https://mhw-db.com/monsters")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("erreur");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    petitmonstre_name(data)
+    grandmonstre_name(data)
+})
 
-// function petitmonstre(data) {
-//     for(n = 0; n < 17 ; n++ ){ 
-//     const monstre_Nom = data[n];
-//     const monstreDiv = document.getElementById("monsterList");
-    // monstre nom
-//     const monstreName = monstre_Nom.name;
-//     const heading = document.createElement("li");
-//     heading.innerHTML = monstreName;
-//     monstreDiv.appendChild(heading);
-//   }
-//   }
-
-//   function grandmonstre(data) {
-//     for(n; n ; n++ ){ 
-//     const monstre_Nom = data[n];
-//     const monstreDiv2 = document.getElementById("grandmonstre");
-    // monstre nom
-//     const monstreName = monstre_Nom.name;
-//     const heading = document.createElement("li");
-//     heading.innerHTML = monstreName;
-//     monstreDiv2.appendChild(heading);
-//   }
-//   }
+function petitmonstre_name(data) {
+    for(n = 0; n < 16 ; n++ ){ 
+    const monstre_Nom = data[n];
+    const monstreDiv1 = document.getElementById("petitmonstre_nom");
+    //monstre nom
+    const monstreName = monstre_Nom.name;
+    const heading = document.createElement("li");
+    heading.innerHTML = monstreName;
+    monstreDiv1.appendChild(heading);
+  }
+  }
+  function grandmonstre_name(data) {
+    for(n; n < 32 ; n++ ){ 
+    let monstre_Nom = data[n];
+    let monstreDiv2 = document.getElementById("grandmonstre_nom");
+    //monstre nom
+    let monstreName = monstre_Nom.name;
+    let heading = document.createElement("li");
+    heading.innerHTML = monstreName;
+    monstreDiv2.appendChild(heading);
+  }
+}
