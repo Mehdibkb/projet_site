c8e2909a86f501667d25259195d448f70605fb99
Guély <sebastien.guely@supdevinci-edu.fr>
2024-02-20 16:02:00 +0100

    modifs des indentations titres pahes maps et histoire

--- armes.js
+++ armes.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(response => response.json())
     .then(data => {
       const armes = data.sort(weapon => weapon.type);
-      console.log(armes);
-      armes_infos(data);
+      armes_infos(armes);
     });
     }
     );
 
 function armes_infos(data) {
+    console.log(data);
+
     for(n = 0; n < 1299; n++ ){ 
         const arme_Nom = data[n];
         const armeDiv = document.getElementById("weaponList");
